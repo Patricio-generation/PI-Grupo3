0bc04a18a0c321d9c59bc8f2cbdee1d6c2743f1b
Romina Rivera <romina.rivera@genstudents.org>
2025-01-28 17:14:18 -0300

    agregue navbar y rutas

---
+++ Pi-Grupo3-React/src/pages/Pagos.jsx
@@ -0,0 +1,9 @@
+function Pagos() {
+  return (
+    <div>
+      <h1>Página de Pagos</h1>
+    </div>
+  );
+}
+
+export default Pagos;
---
+++ Pi-Grupo3-React/src/pages/Reservas.jsx
@@ -0,0 +1,9 @@
+function Reservas() {
+  return (
+    <div>
+      <h1>Página de Reservas</h1>
+    </div>
+  );
+}
+
+export default Reservas;
