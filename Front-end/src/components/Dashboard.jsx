fbe8962c47c04dc45467737cfd1ffa32aa7ff31c
Patricio-generation <patricio.schulz@genstudents.org>
2025-02-06 06:49:57 -0300

    resto de cambios

--- Front-end/src/components/Dashboard.jsx
+++ Front-end/src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../components/Card';
+import Card from './Card';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import '../assets/styles.css';
 
