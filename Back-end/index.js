c4b6e4d47c3cac1095c47f61decbaf87b218c007
Ignacio <ignacio.molina@genstudents.org>
2025-02-12 21:04:29 -0300

    Implementar variables de entorno

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app/app.js'); // importamos app
 const { dbConnection } = require('./src/database/conexion.js'); // importamos conexion
 require('./src/cronJobs/historicalTasks');
-const port = 3000; //constante que almacena el puerto
+const port = process.env.PORT || 3000; //constante que almacena el puerto
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
