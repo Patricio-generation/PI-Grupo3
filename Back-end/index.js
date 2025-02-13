aff3a72c2a6758c1cd3036db1af07599feb05bf2
Patricio-generation <patricio.schulz@genstudents.org>
2025-02-13 09:11:26 -0300

    creacion variable de entorno

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const app = require('./src/app/app.js'); // importamos app
 const { dbConnection } = require('./src/database/conexion.js'); // importamos conexion
 require('./src/cronJobs/historicalTasks');
 const port = process.env.PORT || 3000; //constante que almacena el puerto
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://127.0.0.1:${port}`);
 }); //(listen)=escuchar //traemos variable port
 
 dbConnection();
